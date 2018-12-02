efdbc032ea8430a96555f181a01ee5291e93247e
SP <42236495+SPScientist@users.noreply.github.com>
2018-12-02 16:48:49 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,22 @@
+
+'''
+    This module is used for calc program
+'''
+
+def add(no1,no2):
+    '''
+    This function Will add 2 no's and return
+    :param no1: use int type
+    :param no2: use int type
+    :return: will return int type value
+    '''
+    return no1+no2
+
+def sub(no1,no2):
+    return no1-no2
+
+def mul(no1,no2):
+    return no1*no2
+
+def div(no1,no2):
+    return no1/no2
