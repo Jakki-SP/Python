8bf4ad0c3aefb37023eab6097f13acbde0d6a4c2
SP <42236495+SPScientist@users.noreply.github.com>
2018-12-06 13:52:48 +0530

    Update and rename Lesson (28).py to Lesson028.py

--- Lesson028.py
+++ Lesson028.py
@@ -1,25 +1,25 @@
 
-s1 = "sathya"
+s1 = "scientist"
 s2 = s1.upper()
 print(s1)
 print(s2)
 print("----------------------")
-s1 = "SATHYA"
+s1 = "SCIENTIST"
 s2 = s1.lower()
 print(s1)
 print(s2)
 print("----------------------")
-s1 = "SatHyA"
+s1 = "ScientisT"
 s2 = s1.swapcase()
 print(s1)
 print(s2)
 print("----------------------")
-s1 = "this is sathya from ampt"
+s1 = "what a wonderful day!"
 s2 = s1.title()
 print(s1)
 print(s2)
 print("----------------------")
-s1 = "this is sathya. from ampt"
+s1 = "what a wonderful day!"
 s2 = s1.capitalize()
 print(s1)
 print(s2)
